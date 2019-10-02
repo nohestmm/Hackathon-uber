5033f0ec378531a03361bfe9fc02514e4ef30cb3
nohestmm <nohestmm@gmail.com>
2019-10-02 11:08:00 -0300

    adding arrow

--- uber-app/src/components/Wheel.jsx
+++ uber-app/src/components/Wheel.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import wheel from '../images/wheel.png'
+import arrow from '../images/arrow.png'
 const Wheel = (props) => {
 return(
     <section>
         <h1>Puntos:</h1>
+        <div className="img-arrow">
+           <img 
+           src={arrow}
+           alt="arrow"
+           className="img-responsive"
+           />
+        </div>
         <div className="container-wheel">
             <img 
             id = "img-ruleta"
             src={wheel} 
             alt="wheel"
-            className="img-responsive img-ruleta"
+            className="img-responsive img-wheel"
             style={{
             transform:`rotate(${props.dataWheel}deg)`,
             WebkitTransform:`rotate(${props.dataWheel}deg)`,
@@ -21,7 +29,12 @@ return(
             />
         </div>
         <div className="container-button_spin">
-            <button disabled = {props.animatedWheel} onClick={props.spinWheel}>Girar</button>
+            <div><
+                button disabled = {props.animatedWheel} onClick={props.spinWheel}>Girar</button>
+                </div>
+            <div>
+                <button  onClick={props.unlockSpin}>Habilitar</button>
+                </div>
         </div>
     </section>
 )
--- uber-app/src/views/Game.jsx
+++ uber-app/src/views/Game.jsx
@@ -13,7 +13,7 @@ class Game extends React.Component {
         this.spinWheel = this.spinWheel.bind(this)
         this.stopWheel = this.stopWheel.bind(this)
         this.wheelRef = React.createRef();
-
+        this.unlockSpin = this.unlockSpin.bind(this);
     }
     spinWheel() {
         let wheelTemp = this.wheelData;
@@ -22,65 +22,72 @@ class Game extends React.Component {
         console.log("grado", gradeSpin)
         let prize = gradeSpin / 8;
         console.log("prize", prize)
-        let temporalValue = Math.floor(Math.random()*8);
+        let temporalValue = Math.floor(Math.random() * 8);
         console.log("math", temporalValue)
         let resultWheel = temporalValue * prize;
         console.log("resultado", resultWheel)
         let valuePrize = (gradeSpin * 4) + resultWheel;
         console.log("valor premio", valuePrize)
         this.setState({
-			dataWheel: wheelTemp * prize,
-			animatedWheel: true,
-		})
+            dataWheel: wheelTemp * prize,
+            animatedWheel: true,
+        })
         setTimeout(() => {
-			this.wheelRef.current.classList.add('img-ruleta');
-			this.setState({
-				dataWheel: valuePrize,
-			})
-		}, 200);
+            this.wheelRef.current.classList.add('img-ruleta');
+            this.setState({
+                dataWheel: valuePrize,
+            })
+        }, 200);
     }
 
-    stopWheel(){
-		
-		this.wheelRef.current.classList.remove("img-ruleta");
+    unlockSpin =() => {
+        console.log(this.state.animatedWheel)
+        this.setState({animatedWheel:false})
+      
+    }
+    stopWheel =() =>{
 
-	// 	if (this.pointsWheel >= 0) {
-			
-	this.setState({
-	
-	animatedWheel: false,
-	})
+        this.wheelRef.current.classList.remove("img-ruleta");
 
-	// 	}
-	// 	else{
+        // 	if (this.pointsWheel >= 0) {
 
-	// 		this.setState({
-	// 			animatedWheel: true,
-	// 		})
+        this.setState({
 
-	}
+            animatedWheel: true,
+        })
 
-	// 	if(this.points_data === -1){
-	// 		alert("Felicidades", "Ha ganado un premio!!!", "success");
-	// 	}
-	// 	else if(this.points_data > 0) {
-	// 		alert("Ganó", "Ha ganado " + this.points_data + " puntos", "success");
-	// 	}
-	// 	else {
-	// 		alert("Perdiste", "Inténtelo nuevamente... :( ", "warning");
-	// 	}
+        // 	}
+        // 	else{
 
-	// }
+        // 		this.setState({
+        // 			animatedWheel: true,
+        // 		})
+
+    }
+
+    // 	if(this.points_data === -1){
+    // 		alert("Felicidades", "Ha ganado un premio!!!", "success");
+    // 	}
+    // 	else if(this.points_data > 0) {
+    // 		alert("Ganó", "Ha ganado " + this.points_data + " puntos", "success");
+    // 	}
+    // 	else {
+    // 		alert("Perdiste", "Inténtelo nuevamente... :( ", "warning");
+    // 	}
+
+    // }
 
     render() {
         return (
-            <Wheel 
-            animatedWheel={this.state.animatedWheel}
-            spinWheel = {this.spinWheel}
-            wheelRef = {this.wheelRef}
-            dataWheel = {this.state.dataWheel}
-            stopWheel = {this.stopWheel}/>
-            
+            <Wheel
+                animatedWheel={this.state.animatedWheel}
+                spinWheel={this.spinWheel}
+                wheelRef={this.wheelRef}
+                dataWheel={this.state.dataWheel}
+                stopWheel={this.stopWheel} 
+                unlockSpin = {this.unlockSpin}
+                />
+
         )
     }
 }
